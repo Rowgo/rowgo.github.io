aee4c97e98025fbdaaae1e4d76deaa44f84d572c
Rogan Johnston <100793371+Rowgo@users.noreply.github.com>
2025-05-23 01:43:18 -0700

    popup scrolling
    now when the popup shows up the user can't scroll the page and can scroll the popup if it goes off the top.

--- scripts/popup-binding.js
+++ scripts/popup-binding.js
@@ -7,18 +7,21 @@ function bindPopups(){
     images.forEach((image) => {
         image.onclick = function() {
             popup.style.display = "flex";
+            document.body.style.overflow = "hidden"; // Prevent scrolling
             popupImage.src = this.src;
             popupImage.alt = this.alt;
         }
     });
     closePopup.onclick = function() {
         popup.style.display = "none";
+        document.body.style.overflow = "auto"; // Allow scrolling again
         popupImage.src = "";
         popupImage.alt = "";
     }
     popup.onclick = function(e) {
         if (e.target == popup) {
             popup.style.display = "none";
+            document.body.style.overflow = "auto"; // Allow scrolling again
             popupImage.src = "";
             popupImage.alt = "";
         }
