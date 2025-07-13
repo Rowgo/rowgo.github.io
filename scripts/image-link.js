d6ecee50d3fc11867ad6a607340881e47db3288e
Rogan Johnston <100793371+Rowgo@users.noreply.github.com>
2025-07-12 20:45:52 -0700

    Delete image-link.js

--- scripts/image-link.js
+++
@@ -1,8 +0,0 @@
-class imageLink extends HTMLElement {
-    connectedCallback() {
-        const wrapper = document.createElement("div")
-        const anchor = document.createElement("a")
-
-        wrapper.appendChild(anchor);
-    }
-}
