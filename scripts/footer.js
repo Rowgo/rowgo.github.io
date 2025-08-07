541072fdc113e477d73950927dfff7f7f6fe0b20
Rogan Johnston <100793371+Rowgo@users.noreply.github.com>
2025-08-07 09:17:40 -0700

    made the website mobile friendly

--- scripts/footer.js
+++ scripts/footer.js
@@ -3,7 +3,18 @@
 class Footer extends HTMLElement {
     connectedCallback(){
         this.innerHTML = `
-            <p class="wide-container"> &copy 2025 Rogan Johnston | All Rights Reserved </p>
+            <div class="wide-container"> 
+                    <p class="item-left center-v">&copy 2025 Rogan Johnston | All Rights Reserved</p>               
+                    <a class="item-right inline-link margin-h" href="https://www.youtube.com/@roganjohnston" target="_blank" rel="noopener noreferrer">
+                        <svg class="border-circle icon-small"><use href="media/sprites/socials-icons.svg#icon-youtube"/></svg>
+                    </a>
+                    <a class="inline-link margin-h" href="https://www.twitch.tv/rowgabear" target="_blank" rel="noopener noreferrer">
+                        <svg class="border-circle icon-small"><use href="media/sprites/socials-icons.svg#icon-twitch"/></svg>
+                    </a>
+                    <a class="inline-link margin-h" href="https://www.linkedin.com/in/roganjohnston/" target="_blank" rel="noopener noreferrer">
+                        <svg class="border-circle icon-small"><use href="media/sprites/socials-icons.svg#icon-linkedin"/></svg>
+                    </a>
+            </div>
         `
         this.classList.add('flex')
     }
